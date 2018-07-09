da890847c8d3fa86490e5582b7139ace725ac1f0
PavelDemyanenko <mail@paveldemyanenko.com>
2018-07-09 23:01:31 +0300

    Add gatsby-remark-copy-linked-files

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
+    'gatsby-remark-copy-linked-files',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
